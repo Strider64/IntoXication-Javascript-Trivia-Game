40b2d71aec893294f72c5bef5803af58a6b249cd
John Pepp <jrpepp@pepster.com>
2017-02-12 15:32:16 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'lib/includes/utilities.inc.php';
+?>
+<!DOCTYPE html>
+<!--
+Trivia Game Version 2.0 beta with XML;
+by John Pepp
+Started: January 31, 2017
+Revised: February 12, 2017
+-->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Trivia IntoXication</title>
+        <link rel="stylesheet" href="lib/css/stylesheet.css">
+    </head>
+    <body>
+        <div id="container">
+        </div>
+        <script src="lib/js/trivia.js"></script>
+    </body>
+</html>
