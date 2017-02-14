393935db2fcb548a2c002329d02f94126792677d
John Pepp <jrpepp@pepster.com>
2017-02-14 06:43:21 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ require_once 'lib/includes/utilities.inc.php';
 ?>
 <!DOCTYPE html>
 <!--
-Trivia Game Version 2.0 beta with XML;
+Trivia Game Version 2.02 beta with XML;
 by John Pepp
 Started: January 31, 2017
-Revised: February 12, 2017
+Revised: February 13, 2017
 -->
 <html lang="en">
     <head>
@@ -16,12 +16,21 @@ Revised: February 12, 2017
         <link rel="stylesheet" href="lib/css/stylesheet.css">
     </head>
     <body>
+
+
         <div id="container">
             <div id="heading">
                 <h1>Trivia<span id="toxic">IntoXication</span></h1>
                 <h2 id="subheading">Don't Drive Drunk, Play this Game Instead!</h2>
             </div>
+            <noscript>
+            <div id="noScriptBox">
+                <p class="noscript">Sorry, This quiz requires Javascript to be enabled!</p>
+                <a href="https://www.pepster.com" title="Pepster's Place Website">https://www.pepster.com</a>
+            </div>
+            </noscript>
         </div>
+
         <script src="lib/js/trivia.js"></script>
     </body>
 </html>
